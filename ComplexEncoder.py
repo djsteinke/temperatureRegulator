fcaf331214e955b4a22f9306c14df9c73eccbb04
ds111g <ds111g@att.com>
2021-05-27 15:31:16 -0600

    add recording

--- ComplexEncoder.py
+++ ComplexEncoder.py
@@ -4,6 +4,6 @@ import json
 class ComplexEncoder(json.JSONEncoder):
     def default(self, obj):
         if hasattr(obj, 'repr_json'):
-            return obj.reprJSON()
+            return obj.repr_json()
         else:
             return json.JSONEncoder.default(self, obj)
