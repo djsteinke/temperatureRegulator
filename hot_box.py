bac23659f413ce96c156e69eb70763c64184a7b0
ds111g <ds111g@att.com>
2021-06-30 15:31:38 -0600

    fixes

--- hot_box.py
+++ hot_box.py
@@ -24,9 +24,9 @@ class HotBox(object):
         self._heat_timer = None
         self._status = Status()
         self._history = []
-        self._p_start_time = 0
-        self._s_start_time = 0
-        self._r_start_time = 0
+        self._p_start_time = 0.0
+        self._s_start_time = 0.0
+        self._r_start_time = 0.0
         self._heat = Relay(heat_pin)
         self._vacuum = Relay(vacuum_pin)
         self._callback = None
@@ -171,14 +171,14 @@ class HotBox(object):
         self._hold_timer.start()
 
     def time_in_step(self):
-        if self._s_start_time > 0:
-            return 0
+        if self._s_start_time > 0.0:
+            return 0.0
         else:
             return time.perf_counter() - self._s_start_time
 
     def time_in_program(self):
-        if self._p_start_time > 0:
-            return 0
+        if self._p_start_time > 0.0:
+            return 0.0
         else:
             return time.perf_counter() - self._p_start_time
 
